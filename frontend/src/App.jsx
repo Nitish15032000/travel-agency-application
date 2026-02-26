e3d37ab788db96e3111c69e1af906a394dedbef9
Nitish Kumar <nitish.edu.in@gmail.com>
2026-02-26 23:37:01 +0530

    Frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-blue-300 flex justify-center items-center h-16 font-bold'> Travel Agency</div>
+    </>
+  )
+}
+
+export default App
